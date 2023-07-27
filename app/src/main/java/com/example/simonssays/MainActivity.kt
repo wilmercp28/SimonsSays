5941d403ac6c40a22aa08730e084ea59f2a636cd
wilmer <wilmercp28@gmail.com>
2023-07-27 12:51:38 -0400

    Hello

--- app/src/main/java/com/example/simonssays/MainActivity.kt
+++ app/src/main/java/com/example/simonssays/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.simonssays
 
 import android.os.Bundle
-import android.util.Log
-import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,38 +8,52 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simonssays.ui.theme.SimonsSaysTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import java.util.Stack
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    private val generatedSequence = mutableListOf<Pair<Int, Int>>()
+    private val simonSequence = mutableListOf<Pair<Int, Int>>()
+    private var isPlayerTurn by mutableStateOf(false)
+    private var level by mutableStateOf(1)
+    private var playerLost by mutableStateOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -56,34 +68,48 @@ class MainActivity : ComponentActivity() {
                     repeat(9) { index ->
                         boxColors.add(Color.Green)
                     }
-
+                    if (playerLost){
+                        losingScreen()
+                    } else {
                     Createbox(boxColors) { row, column ->
-                        printButtonPosition(row, column)
                         ChangeBoxColor(boxColors, row, column)
-
                     }
                     LaunchedEffect(true) {
                         SimonsSaysActions(boxColors)
                     }
+                    }
+
             }
         }
     }
 
 }
 
+    @Preview
+    @Composable
+    fun CreateboxPreview() {
+        val boxColors = remember { mutableStateListOf<Color>() }
+        repeat(9) { index ->
+            boxColors.add(Color.Green)
+        }
+        var isPlayerTurn by remember { mutableStateOf(true) } // Start with the player's turn
+        var level by remember { mutableStateOf(1) }
 
-    //@Preview
-    //@Composable
-   // fun CreateboxPreview() {
-     //   Createbox { row, column, boxColors: MutableList<Color> ->
-            // Handle button click logic here in the preview
-       //     println("Button at row $row and column $column was clicked in the preview.")
-        //}
-    //}
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Createbox(boxColors) { row, column ->
+                // This is a mock click handler for preview. You can remove this if needed.
+                println("Button at row $row and column $column was clicked in the preview.")
+            }
+        }
+    }
+
+
 
     @Composable
     fun Createbox(boxColors: MutableList<Color>, onButtonClick: (row: Int, column: Int) -> Unit) {
-
         repeat(9) { index ->
             boxColors.add(Color.Green)
         }
@@ -95,10 +121,29 @@ class MainActivity : ComponentActivity() {
             Arrangement.Center,
             Alignment.CenterHorizontally,
         ) {
+            Box(
+
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Green, shape = RoundedCornerShape(20.dp))
+                    .border(5.dp, Color.Black, shape = RoundedCornerShape(20.dp)),
+                contentAlignment = Alignment.Center
+                ){
+                   Text(
+                       text = "Level\n$level",
+                       textAlign = TextAlign.Center,
+                       color = Color.Black,
+                       style = TextStyle(
+                           fontSize = 30.sp,
+                           fontFamily = FontFamily.Monospace)
+                   )
+
+            }
+            Spacer(modifier = Modifier.size(100.dp))
+            var clickCounter by remember { mutableStateOf(0) }
             for (x in 0 until 3) {
                 Row(
                     horizontalArrangement = Arrangement.Center
-
                 ) {
                     for (y in 0 until 3) {
                         val index = x * 3 + y
@@ -109,41 +154,94 @@ class MainActivity : ComponentActivity() {
                                 .border(5.dp, Color.Black, shape = RoundedCornerShape(20.dp))
                                 .background(boxColors[index], shape = RoundedCornerShape(20.dp))
                                 .clickable {
-                                    //Testing Button positions (Remove After testing)
-                                    printButtonPosition(x, y)
-                                    ChangeBoxColor(boxColors, x, y)
-                                    playerSequence.add(Pair(x, y))
+                                    if (isPlayerTurn) {
+                                        clickCounter++
+                                        ChangeBoxColor(boxColors, x, y)
+                                        playerSequence.add(Pair(x, y))
+                                        if (clickCounter == level) {
+                                            isPlayerTurn = false
+                                            checkForWinner(simonSequence, playerSequence, boxColors)
+                                            clickCounter = 0
+                                        }
+                                    }
+
                                 }
                         )
-                        if (playerSequence == generatedSequence) {
-
-                        } else {
-
-                        }
 
                     }
                 }
             }
         }
     }
+    @Preview
+    @Composable
+    fun losingScreen() {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
 
-    //testing buttons position ( Remove after testing)
-    private fun printButtonPosition(row: Int, column: Int) {
-        // Print the position of the button that was clicked
-        println("Button at row $row and column $column was clicked.")
+            ) {
+
+        }
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Box(
+                modifier = Modifier
+                    .size(width = 300.dp, height = 100.dp)
+                    .background(Color.Green, shape = RoundedCornerShape(20.dp))
+                    .border(5.dp, Color.Black, shape = RoundedCornerShape(20.dp))
+
+                ){
+                Text(text = "Restar")
+            }
+        }
     }
 
+    fun checkForWinner(
+        simonSequence: MutableList<Pair<Int, Int>>,
+        playerSequence: SnapshotStateList<Pair<Int, Int>>,
+        boxColors: MutableList<Color>
+    ) {
+        if (simonSequence.equals(playerSequence)){
+            simonSequence.clear()
+            playerSequence.clear()
+            level++
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(200)
+            SimonsSaysActions(boxColors)
+            }
+        } else {
+            playerLost = true
+
+        }
+        simonSequence.clear()
+        playerSequence.clear()
+    }
     fun SimonsSaysActions(boxColors: MutableList<Color>) {
-        var level: Int
-        level = 3
         CoroutineScope(Dispatchers.Main).launch {
+            delay(500)
             repeat(level) {
                 val newRow = Random.nextInt(3)
                 val newColumn = Random.nextInt(3)
                 ChangeBoxColor(boxColors, newRow, newColumn)
-                generatedSequence.add(Pair(newRow, newColumn)) // Add the random click to the generated sequence
-                delay(1000)
+                simonSequence.add(Pair(newRow, newColumn)) // Add the random click to the generated sequence
+delay(200)
             }
+            isPlayerTurn = true
+            //Remove after testing
+            printSimonSequence(simonSequence)
+        }
+
+    }
+
+    //Remove after Testing
+    fun printSimonSequence(sequence: List<Pair<Int, Int>>) {
+        println("Simon Sequence:")
+        for ((row, column) in sequence) {
+            println("Row: $row, Column: $column")
         }
     }
 
@@ -153,7 +251,7 @@ class MainActivity : ComponentActivity() {
 
         // Use a coroutine to delay the color change back to green after 1 second
         CoroutineScope(Dispatchers.Main).launch {
-            delay(300)
+            delay(200)
             boxColors[index] = Color.Green // Change the color back to green after the delay
         }
     }
